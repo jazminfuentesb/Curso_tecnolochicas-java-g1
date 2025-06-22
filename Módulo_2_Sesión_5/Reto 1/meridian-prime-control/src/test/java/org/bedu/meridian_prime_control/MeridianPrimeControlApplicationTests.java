6f11ffa73c28cca4e2180d4546c225fcc80ea720
Jazmín Fuentes <140033691+jazminfuentesb@users.noreply.github.com>
2025-06-22 17:16:44 -0600

    Add files via upload

---
+++ Módulo_2_Sesión_5/Reto 1/meridian-prime-control/src/test/java/org/bedu/meridian_prime_control/MeridianPrimeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.meridian_prime_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridianPrimeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
